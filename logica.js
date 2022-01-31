58addd800778ab76fba8d060d12652c6dcb6730e
Lainezz <diegolinaresmojacar@gmail.com>
2022-01-31 16:32:18 +0100

    commit cambio objeto xmlhttp

--- logica.js
+++ logica.js
@@ -8,23 +8,20 @@ function inicializarPagina() {
 function doPetAJAX() {
     const objXHR = obtainXMLHttpRequest();
 
-     // Definimos el comportamiendo del evento onreadystatechange
-    objXHR.onreadystatechange= () => 
-    {
-		// Si el estado es uno, la capa cambia a CARGANDO
-		if (objXHR.readyState===1) {
-                console.log("cargando");
-		     	document.getElementById('capa').innerHTML="CARGANDO...";
-		}
-		// Si el estado es 4 y se ha hecho la conexion correcta, se muestra en la capa la respuesta recibida
-		if (objXHR.readyState===4) 
-		{
-		    if (objXHR.status===200)
-		     {
+    // Definimos el comportamiendo del evento onreadystatechange
+    objXHR.onreadystatechange = () => {
+        // Si el estado es uno, la capa cambia a CARGANDO
+        if (objXHR.readyState === 1) {
+            console.log("cargando");
+            document.getElementById('capa').innerHTML = "CARGANDO...";
+        }
+        // Si el estado es 4 y se ha hecho la conexion correcta, se muestra en la capa la respuesta recibida
+        if (objXHR.readyState === 4) {
+            if (objXHR.status === 200) {
                 console.log("realizado");
-		     	document.getElementById('capa').innerHTML=objXHR.responseText;
-		     }
-		}
+                document.getElementById('capa').innerHTML = objXHR.responseText;
+            }
+        }
     }
 
     //En la funcionalidad Open establecemos el método de envío, la url, y
@@ -34,7 +31,7 @@ function doPetAJAX() {
     //Establecemos parámetros que irán en la cabecera
     //En las peticiones POST tenemos que enviar en la cabecera el COntent-Type
     //ya que los datos se envían formando parte de la cabecera
-    objXHR.setRequestHeader("Content-Type", "application/x-www-form-urlencoded", );   
+    objXHR.setRequestHeader("Content-Type", "application/x-www-form-urlencoded",);
 
     // Aqui enviamos la peticion síncrona
     objXHR.send(null);
@@ -56,16 +53,14 @@ function obtainXMLHttpRequest() {
             httpRequest = new ActiveXObject("MSXML2.XMLHTTP");
         } catch (e) {
             try {
-                httpRequest = new
-                    ActiveXObject("Microsoft.XMLHTTP");
+                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
             } catch (e) { }
         }
     }
     // Si no se puede crear, devolvemos false. En caso contrario, devolvemos el objeto
     if (!httpRequest) {
         return false;
-    }
-    else {
+    } else {
         return httpRequest;
     }
 }
